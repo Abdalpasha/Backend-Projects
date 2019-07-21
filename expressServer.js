d631e5e75b9f03697a818ed8e1777247837184dd
Abdal Pasha <abdalpasha1995@gmail.com>
2019-07-21 17:02:57 +0530

    db connection

--- expressServer.js
+++ expressServer.js
@@ -1,11 +1,28 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
 var ExpressApp = /** @class */ (function () {
     function ExpressApp() {
+        this.mongourl = "mongodb://localhost:27017/mydb";
+        //code to eshtablish a local server
         this.app = express();
         this.app.listen(3000, 'localhost', function () { console.log("server started"); });
+        this.configBodyParser();
+        this.connectDb();
     }
+    ExpressApp.prototype.configBodyParser = function () {
+        //code to view the Body
+        this.app.use(bodyparser.json());
+        this.app.use(bodyparser.urlencoded({ extended: false }));
+    };
+    ExpressApp.prototype.connectDb = function () {
+        //code to connect MongoDB
+        var mongooseOption = { useNewUrlParser: true };
+        mongoose.connect(this.mongourl, mongooseOption)
+            .then(function (data) { return console.log("db connection success"); })["catch"](function (error) { console.log(error); console.log("Db connection failure"); });
+    };
     return ExpressApp;
 }());
 var myApp = new ExpressApp();
@@ -26,4 +43,8 @@ myApp.app.get('/getTodoBYId/:todoId/user/:userId', function (req, res) {
     console.log(req.query);
     console.log(req.statusCode);
     res.send("get API by Id");
+});
+myApp.app.post('/createTodo', function (req, res) {
+    console.log(req.body);
+    res.send("Todo Created");
 });
--- expressServer.ts
+++ expressServer.ts
@@ -1,18 +1,40 @@
 import * as express from "express";
-class ExpressApp{
-    app:express.Application;
-    constructor(){
+import * as bodyparser from "body-parser";
+import * as mongoose from "mongoose";
+
+
+class ExpressApp {
+    app: express.Application;
+    mongourl: string = "mongodb://localhost:27017/mydb"
+    constructor() {
+        //code to eshtablish a local server
         this.app = express();
-        this.app.listen(3000,'localhost',()=>{console.log("server started")})
+        this.app.listen(3000, 'localhost', () => { console.log("server started") });
+        this.configBodyParser();
+        this.connectDb();
     }
+
+    private configBodyParser(){
+        //code to view the Body
+        this.app.use(bodyparser.json());
+        this.app.use(bodyparser.urlencoded({extended:false}));
+    }
+    private connectDb(){
+        //code to connect MongoDB
+        let mongooseOption : mongoose.ConnectionOptions = {useNewUrlParser : true}
+        mongoose.connect(this.mongourl,mongooseOption)
+        .then((data) => console.log("db connection success"))
+        .catch(error => {console.log(error);console.log("Db connection failure")})
+    }
+
 }
 
 let myApp = new ExpressApp();
-myApp.app.get('/',(req,res)=>{res.send("server is running")})
+myApp.app.get('/', (req, res) => { res.send("server is running") })
 
-myApp.app.get('/task',(req:express.Request,res:express.Response)=>{res.send("this is new server")})
+myApp.app.get('/task', (req: express.Request, res: express.Response) => { res.send("this is new server") })
 
-myApp.app.get('/getAlltodo',(req:express.Request,res:express.Response)=>{
+myApp.app.get('/getAlltodo', (req: express.Request, res: express.Response) => {
     console.log(req.method);
     console.log(req.url);
     console.log(req.params);
@@ -21,11 +43,15 @@ myApp.app.get('/getAlltodo',(req:express.Request,res:express.Response)=>{
     res.send("get All API Called");
 })
 
-myApp.app.get('/getTodoBYId/:todoId/user/:userId',(req:express.Request,res:express.Response)=>{
+myApp.app.get('/getTodoBYId/:todoId/user/:userId', (req: express.Request, res: express.Response) => {
     console.log(req.method);
     console.log(req.url);
     console.log(req.params);
     console.log(req.query);
     console.log(req.statusCode)
     res.send("get API by Id");
+})
+myApp.app.post('/createTodo', (req: express.Request, res: express.Response) => {
+    console.log(req.body)
+    res.send("Todo Created")
 })
